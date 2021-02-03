4e454c1dfa2586c3706a3fb70011c50a2564153b
Ryan Gerstenkorn <ryan.gerstenkorn@rhinosecuritylabs.com>
2021-02-02 22:34:18 -0600

    Cleanup snapshot.py

--- pysnap/snapshot.py
+++ pysnap/snapshot.py
@@ -1,6 +1,6 @@
 import os
 from threading import Thread
-from typing import TYPE_CHECKING, List, Iterator, NamedTuple, IO, Callable
+from typing import TYPE_CHECKING, List, Iterator, NamedTuple, IO, Callable, Generator
 import logging
 from queue import Queue, Empty
 
@@ -18,11 +18,10 @@ import boto3.session
 MEGABYTE: int = 1024 * 1024
 GIBIBYTE: int = 1024 * MEGABYTE
 
-FETCH_THREADS=30
+FETCH_THREADS = 30
 
 
-class WriteBlock(NamedTuple):
-    File: str
+class Block(NamedTuple):
     BlockData: IO[bytes]
     Offset: int
 
@@ -32,49 +31,47 @@ class Snapshot:
         self.snapshot_id = snapshot_id
         self.ebs: EBSClient = sess.client('ebs', config=botocore.config.Config(max_pool_connections=FETCH_THREADS))
         self.ec2: EC2Client = sess.client('ec2')
-        self.volume_size = 0
-        self.data_size_mb = 0
-        self.received_mb = 0
-        self.block_size_bytes = 0
+
         self.queue = Queue()
-        self.workers = 5
         self.output_file = None
+
+        self.volume_size_b = 0
+        self.total_blocks = 0
+        self.blocks_written = 0
+        self.block_size_b = 0
 
         self.get_blocks()
 
     def get_blocks(self) -> List[BlockTypeDef]:
         resp = self.ebs.list_snapshot_blocks(SnapshotId=self.snapshot_id)
-        self.block_size_bytes = resp['BlockSize']
-        self.volume_size = resp['VolumeSize'] * GIBIBYTE
-        logging.info(f"Volume size is {self.volume_size}")
 
-        self.data_size_mb += int(self.block_size_bytes * len(resp['Blocks']) / MEGABYTE)
+        self.block_size_b = resp['BlockSize']
+        self.volume_size_b = resp['VolumeSize'] * GIBIBYTE
+        logging.info(f"Volume size is {self.volume_size_b}")
 
-        next_token = resp.get('NextToken')
         blocks = resp['Blocks']
-        while next_token:
-            resp = self.ebs.list_snapshot_blocks(SnapshotId=self.snapshot_id, NextToken=next_token)
-            next_token = resp.get('NextToken')
-            blocks.extend(resp['Blocks'])
-            self.data_size_mb += int(resp['BlockSize'] * len(resp['Blocks']) / MEGABYTE)
 
-        logging.info(f"Number of blocks in image: {len(blocks)}")
+        while resp.get('NextToken'):
+            resp = self.ebs.list_snapshot_blocks(SnapshotId=self.snapshot_id, NextToken=next_token)
+            blocks.extend(resp['Blocks'])
+
+        self.total_blocks = len(blocks)
+        logging.info(f"Number of blocks in image: {self.total_blocks}")
+
         return blocks
 
     def download(self, output_file: str):
-        if not output_file:
-            output_file = f"{self.snapshot_id}.img"
+        assert output_file
         self.output_file = os.path.abspath(output_file)
 
         with open(output_file, 'wb') as f:
-            logging.info(f"Truncating file to {self.volume_size}")
-            f.truncate(self.volume_size)
+            logging.info(f"Truncating file to {self.volume_size_b}")
+            f.truncate(self.volume_size_b)
             f.flush()
 
         threads = list()
-        done = False
         for i in range(FETCH_THREADS):
-            t = Thread(target=self._write_blocks_worker, args=(lambda: done,))
+            t = Thread(target=self._write_blocks_worker)
             threads.append(t)
             t.start()
 
@@ -84,20 +81,19 @@ class Snapshot:
             self.queue.put(block)
 
         self.queue.join()
-        done = True
 
         for t in threads:
             t.join()
 
         print(f"Output Path: {output_file}")
 
-    def _write_blocks_worker(self, done: Callable):
-        while not self.queue.empty() or not done():
+    def _write_blocks_worker(self):
+        while self.total_blocks != self.blocks_written:
             try:
-                block: BlockTypeDef = self.queue.get(timeout=1)
-                write_block: WriteBlock = self.fetch_block(block)
-                self.received_mb += self.write_block(write_block) / MEGABYTE
-                print(f"Received {self.received_mb} of {self.data_size_mb}", end='\r')
+                block: BlockTypeDef = self.queue.get(timeout=0.2)
+                self.write_block(self.fetch_block(block))
+                self.blocks_written += 1
+                print(f"Saved block {self.blocks_written} of {self.total_blocks}", end='\r')
                 self.queue.task_done()
             except Exception as e:
                 if isinstance(e, Empty):
@@ -106,7 +102,7 @@ class Snapshot:
                     logging.exception(f"[ERROR] {e.args}")
                     raise e
 
-    def fetch_block(self, block: BlockTypeDef) -> WriteBlock:
+    def fetch_block(self, block: BlockTypeDef) -> Block:
         logging.debug(f"Getting block index {block['BlockIndex']}")
         resp = self.ebs.get_snapshot_block(
             SnapshotId=self.snapshot_id,
@@ -116,17 +112,15 @@ class Snapshot:
 
         # BlockIndex is equal to 512 KiB and seek uses bytes.
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ebs.html#EBS.Client.put_snapshot_block
-        return WriteBlock(
-            File=self.output_file,
-            Offset=block['BlockIndex'] * self.block_size_bytes,
+        return Block(
+            Offset=block['BlockIndex'] * self.block_size_b,
             BlockData=resp['BlockData'],
         )
 
-    @staticmethod
-    def write_block(block: WriteBlock):
+    def write_block(self, block: Block) -> int:
         logging.debug(f"Writing block at offset {block.Offset}")
         """Takes a WriteBlock object to write to disk and yields the number of MiB's for each write."""
-        with os.fdopen(os.open(block.File, os.O_RDWR | os.O_CREAT), 'rb+') as f:
+        with os.fdopen(os.open(self.output_file, os.O_RDWR | os.O_CREAT), 'rb+') as f:
             f.seek(block.Offset)
             bytes_written = f.write(block.BlockData.read())
             f.flush()
